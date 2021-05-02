a1c473a8dad6f5339ecd0708601a602046d30cea
Owen Nicholson <nicholowen@ecs.vuw.ac.nz>
2021-05-02 13:16:07 +1200

    First and final commit.

---
+++ Part 2/src/Part2/GPMathProblem.java
@@ -0,0 +1,109 @@
+package Part2;
+
+import org.jgap.InvalidConfigurationException;
+import org.jgap.gp.CommandGene;
+import org.jgap.gp.GPProblem;
+import org.jgap.gp.function.*;
+import org.jgap.gp.impl.DeltaGPFitnessEvaluator;
+import org.jgap.gp.impl.GPConfiguration;
+import org.jgap.gp.impl.GPGenotype;
+import org.jgap.gp.terminal.Terminal;
+import org.jgap.gp.terminal.Variable;
+
+/**
+ * @author Owen Nicholson - 300130635
+ * @credit carlos - tutorial code supplied by lecturer
+ *
+ */
+public class GPMathProblem extends GPProblem {
+  @SuppressWarnings("boxing")
+  private static double[] inputs = { -2.00, -1.75, -1.50, -1.25, -1.00, -0.75, -0.50, -0.25, 0.00, 0.25, 0.50, 0.75, 1.00, 1.25, 1.50, 1.75, 2.00, 2.25, 2.50, 2.75 };
+
+  private static double[] outputs = { 37.00000, 24.16016, 15.06250, 8.91016, 5.00000, 2.72266, 1.56250, 1.09766, 1.00000, 1.03516, 1.06250, 1.03516, 1.00000, 1.09766, 1.56250, 2.72266, 5.00000, 8.91016, 15.06250, 24.16016 };
+
+  private Variable var;
+
+  public GPMathProblem() throws InvalidConfigurationException {
+    super(new GPConfiguration());
+
+    GPConfiguration config = getGPConfiguration();
+
+    var = Variable.create(config, "x", CommandGene.DoubleClass);
+
+    config.setGPFitnessEvaluator(new DeltaGPFitnessEvaluator());
+    config.setMaxInitDepth(4);
+    config.setPopulationSize(800);
+    config.setMutationProb(0.1f);
+    config.setCrossoverProb(0.8f);
+    config.setMaxCrossoverDepth(6);
+    config.setFitnessFunction(new GPMathProblemFitnessFunction(inputs, outputs, var));
+    config.setStrictProgramCreation(true);
+  }
+
+  @Override
+  public GPGenotype create() throws InvalidConfigurationException {
+    GPConfiguration config = getGPConfiguration();
+
+    // The return type of the GP program.
+    Class[] types = { CommandGene.DoubleClass };
+
+    // Arguments of result-producing chromosome: none
+    Class[][] argTypes = { {} };
+
+    // Next, we define the set of available GP commands and terminals to
+    // use.
+    CommandGene[][] nodeSets = {
+          {
+                var,
+                new Add(config, CommandGene.DoubleClass),
+                new Multiply(config, CommandGene.DoubleClass),
+                new Subtract(config, CommandGene.DoubleClass),
+                new Pow(config, CommandGene.DoubleClass), // pow added to prevent the system to have to generate it
+                new Terminal(config, CommandGene.DoubleClass, 0.0, 4.0, true) // Random constant variable between the range 0.0 and 4.0
+          }
+    };
+
+    GPGenotype result = GPGenotype.randomInitialGenotype(config, types, argTypes, nodeSets, 20, true);
+
+    return result;
+  }
+
+  public static void main(String[] args) throws Exception {
+
+    int min = -1;
+    int max = 0;
+    GPProblem problem = new GPMathProblem();
+
+    int generations = 100;
+
+    /*
+      Iterates over a number of evolutions, 1 at a time, allowing me to check each generation and to determine when the fitness == 0 (correct solution)
+
+      Commented out lines were used for testing - performs 200 loops, stopping after it finds a best solution and recording how many generations it took.
+      Then prints the min and max number of generations.
+    */
+
+//    for(int r = 0; r < 200; r++) {
+
+    GPGenotype gp = problem.create();
+    for (int i = 0; i < generations; i++) {
+      gp.setVerboseOutput(true);
+      gp.evolve(1); // will evolve a single generation each time
+
+//        if (gp.getFittestProgram().getFitnessValue() == 0) {
+//          if(min == -1){
+//            min = i; max = i;
+//          }
+//          if(i < min) min = i;
+//          if(i > max) max = i;
+//          break;
+//        }
+      gp.outputSolution(gp.getAllTimeBest());
+    }
+//    }
+//    System.out.println("Min generations: " + min);
+//    System.out.println("Max generations: " + max);
+
+  }
+
+}
---
+++ Part 2/src/Part2/GPMathProblemFitnessFunction.java
@@ -0,0 +1,48 @@
+package Part2;
+
+import org.jgap.gp.GPFitnessFunction;
+import org.jgap.gp.IGPProgram;
+import org.jgap.gp.terminal.Variable;
+
+/**
+ * @author Owen Nicholson - 300130635
+ * @credit carlos - tutorial code supplied by lecturer
+ *
+ */
+public class GPMathProblemFitnessFunction extends GPFitnessFunction {
+
+  private double[] inputs;
+  private double[] outputs;
+  private Variable var;
+
+  private static Object[] NO_ARGS = new Object[0];
+
+  public GPMathProblemFitnessFunction(double[] input, double[] output, Variable x) {
+    inputs = input;
+    outputs = output;
+    var = x;
+  }
+
+  @Override
+  protected double evaluate(final IGPProgram program) {
+    double result;
+
+    long error = 0;
+
+    for (int i = 0; i < inputs.length; i++) {
+      // Set the input values
+      var.set(inputs[i]);
+      // Execute the genetically engineered algorithm
+      long value = (long)program.execute_double(0, NO_ARGS);
+
+//      error += Math.abs(value - outputs[i]); // Absolute error
+
+      error += Math.pow((long)(value - outputs[i]), 2); //Squared Error
+    }
+
+    result = error * (1/(double)inputs.length); // Mean squared error
+
+    return result;
+  }
+
+}
